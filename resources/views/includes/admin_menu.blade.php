5dd10250b81dc8975df5c45a709309143efa03b3
Asif Khan <asifkhan.github@gmail.com>
2022-09-06 23:48:56 +0600

    admin menu modified

--- resources/views/includes/admin_menu.blade.php
+++ resources/views/includes/admin_menu.blade.php
@@ -8,180 +8,52 @@
       </li>
       <li class="nav-item has-treeview">
         <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-swatchbook"></i>
-          <p>Sales<i class="fas fa-angle-left right"></i></p>
-        </a>
-        <ul class="nav nav-treeview">
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Sale List</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>New Sale</p>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item has-treeview">
-        <a href="#" class="nav-link">
           <i class="nav-icon fas fa-sitemap"></i>
-          <p>Products<i class="fas fa-angle-left right"></i></p>
+          <p>Candidates<i class="fas fa-angle-left right"></i></p>
         </a>
         <ul class="nav nav-treeview">
           <li class="nav-item">
             <a href="categories.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Categories</p>
+              <p>All Candidates</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="brands.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Brands</p>
+              <p>Approved</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="products.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Products</p>
+              <p>Rejected</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="create_product.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Create Product</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="print_barcode.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Print Barcode</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="wastages.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Wastage List</p>
+              <p>Pending</p>
             </a>
           </li>
         </ul>
       </li>
       <li class="nav-item has-treeview">
         <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-shopping-bag"></i>
-          <p>
-            Purchase
-            <i class="right fas fa-angle-left"></i>
-          </p>
-        </a>
-        <ul class="nav nav-treeview">
-          <li class="nav-item">
-            <a href="purchase_list.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Purchase List</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="add_purchase.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>New Purchase</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="suppliers.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Suppliers</p>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item has-treeview">
-        <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-donate"></i>
-          <p>
-            Expenses
-            <i class="fas fa-angle-left right"></i>
-          </p>
-        </a>
-        <ul class="nav nav-treeview">
-          <li class="nav-item">
-            <a href="expense_categories.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Expense Categories</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="expenses.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Expenses</p>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item has-treeview">
-        <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-layer-group"></i>
-          <p>
-            Stock Management
-            <i class="fas fa-angle-left right"></i>
-          </p>
+          <i class="nav-icon fas fa-swatchbook"></i>
+          <p>Quiz Management<i class="fas fa-angle-left right"></i></p>
         </a>
         <ul class="nav nav-treeview">
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Stock In</p>
+              <p>Quiz List</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Stock Out/Invoice</p>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item">
-        <a href="users.html" class="nav-link">
-          <i class="nav-icon fas fa-users"></i>
-          <p>Users</p>
-        </a>
-      </li>
-      <li class="nav-item has-treeview">
-        <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-cog"></i>
-          <p>
-            Settings
-            <i class="fas fa-angle-left right"></i>
-          </p>
-        </a>
-        <ul class="nav nav-treeview">
-          <li class="nav-item">
-            <a href="taxes.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Tax</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="units.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Units</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="company_profile.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Company Profile</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="stores.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Stores</p>
+              <p>New Quiz</p>
             </a>
           </li>
         </ul>
