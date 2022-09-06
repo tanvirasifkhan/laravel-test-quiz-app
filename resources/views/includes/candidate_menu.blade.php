e89288fa757494dc2c6de5a733950b39b8540cac
Asif Khan <asifkhan.github@gmail.com>
2022-09-06 23:52:08 +0600

    menu created for candidate

---
+++ resources/views/includes/candidate_menu.blade.php
@@ -0,0 +1,30 @@
+<nav>
+    <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-widget="treeview" role="menu" data-accordion="false">
+      <li class="nav-item">
+        <a href="dashboard.html" class="nav-link active">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>Dashboard</p>
+        </a>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-swatchbook"></i>
+          <p>Quiz Participation<i class="fas fa-angle-left right"></i></p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Participation List</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>New Participation</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+</nav>
