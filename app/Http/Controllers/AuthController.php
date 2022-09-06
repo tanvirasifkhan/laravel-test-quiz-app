8d2ab3db4821a1df2534a2a31a2be93af67ef3c3
Asif Khan <asifkhan.github@gmail.com>
2022-09-06 21:51:42 +0600

    controller created for authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Candidate;
+use Validator;
+
+class AuthController extends Controller {
+    
+}
