6455a08dd425dfc630624c1331639b3a1ebce251
Asif Khan <asifkhan.github@gmail.com>
2022-09-07 20:56:59 +0600

    dashboard page state cards  modified

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -8,10 +8,7 @@
           <h1 class="m-0 text-dark">Dashboard</h1>
         </div>
         <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Dashboard</li>
-          </ol>
+          <ol class="breadcrumb float-sm-right"></ol>
         </div>
       </div>
     </div>
@@ -23,48 +20,44 @@
           <div class="small-box bg-info">
             <div class="inner">
               <h3>150</h3>
-              <p>New Orders</p>
+              <p>Total Quizes</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-6">
-          <div class="small-box bg-success">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <div class="col-lg-3 col-6">
           <div class="small-box bg-warning">
             <div class="inner">
               <h3>44</h3>
-              <p>User Registrations</p>
+              <p>Total Candidates</p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>53</h3>
+              <p>Approved Candidates</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
           </div>
         </div>
         <div class="col-lg-3 col-6">
           <div class="small-box bg-danger">
             <div class="inner">
               <h3>65</h3>
-              <p>Unique Visitors</p>
+              <p>Pending</p>
             </div>
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
       </div>
