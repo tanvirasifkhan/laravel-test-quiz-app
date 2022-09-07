9ffcddaad7562076c569ea5032e64b7128c2740d
Asif Khan <asifkhan.github@gmail.com>
2022-09-07 22:08:09 +0600

    authenticate method created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Candidate;
 use Validator;
+use Auth;
 
 class AuthController extends Controller {
     
@@ -17,5 +18,23 @@ class AuthController extends Controller {
     // register view page
     public function registerView(){
         return view('auth.register');
+    }
+
+    // log the user into the dashboard
+    public function authenticate(Request $request){
+        $validators=Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        if($validators->fails()){
+            return redirect()->route('auth.login_view')->withErrors($validators)->withInput();
+        }else{
+            if(Auth::guard('admin')->attempt(['email'=>$request->email,'password'=>$request->password])){
+                return redirect()->route('admin.dashboard')->with('message','Successfully loggedin !');             
+            }else{
+                return redirect()->route('auth.login_view')->with('error_message','Email/Password is wrong !'); 
+            }
+        }           
     }
 }
