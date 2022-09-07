06a599bdf270e12a0c0f13e86d96d8cc90cf4417
Asif Khan <asifkhan.github@gmail.com>
2022-09-07 21:14:11 +0600

    guard created for admin & candidate

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,16 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'candidate' => [
+            'driver' => 'session',
+            'provider' => 'candidates',
+        ],
     ],
 
     /*
@@ -65,10 +75,10 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'candidates' => [
+            'driver' => 'database',
+            'model' => App\Models\Candidate::class,
+        ],
     ],
 
     /*
