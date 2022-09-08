4ed35d49dc2fcd01c16a97b39d1ac1919c50c219
Asif Khan <asifkhan.github@gmail.com>
2022-09-08 20:05:45 +0600

    method created for candidate signup & condition added for authentication redirection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,6 +20,31 @@ class AuthController extends Controller {
         return view('auth.register');
     }
 
+    // register candidate
+    public function register(Request $request){
+        $validators = Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|email|unique:candidates,email|unique:users,email',
+            'phone'=>'required|numeric|digits:11|unique:candidates',
+            'cv_link'=>'required',
+            'password'=>'required'
+        ]);
+
+        if($validators->fails()){
+            return redirect()->route('register')->withErrors($validators)->withInput();
+        }else{
+            $candidate = new Candidate();
+            $candidate->name = $request->name;
+            $candidate->email = $request->email;
+            $candidate->phone = $request->phone;
+            $candidate->cv_link = $request->cv_link;
+            $candidate->password = bcrypt($request->password);
+            $candidate->status = 'pending';
+            $candidate->save();
+            return redirect()->route('login')->with('message','Successfully registered.You can login now !');
+        }        
+    }
+
     // log the user into the dashboard
     public function authenticate(Request $request){
         $validators=Validator::make($request->all(),[
@@ -28,10 +53,12 @@ class AuthController extends Controller {
         ]);
 
         if($validators->fails()){
-            return redirect()->route('.login')->withErrors($validators)->withInput();
+            return redirect()->route('login')->withErrors($validators)->withInput();
         }else{
             if(Auth::guard('admin')->attempt(['email'=>$request->email,'password'=>$request->password])){
                 return redirect()->route('admin.dashboard')->with('message','Successfully loggedin !');             
+            }elseif(Auth::guard('candidate')->attempt(['email'=>$request->email,'password'=>$request->password])){
+                return redirect()->route('candidate.dashboard')->with('message','Successfully loggedin !');             
             }else{
                 return redirect()->route('login')->with('error_message','Email/Password is wrong !'); 
             }
