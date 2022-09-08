fcb052457a01b683e0027e707c4b7a4e69989de8
Asif Khan <asifkhan.github@gmail.com>
2022-09-08 17:32:50 +0600

    total mark column added

--- database/migrations/2022_09_06_142323_create_quiz_table.php
+++ database/migrations/2022_09_06_142323_create_quiz_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('question_options');
+            $table->integer('total_mark');
             $table->integer('pass_mark');
             $table->timestamps();
         });
