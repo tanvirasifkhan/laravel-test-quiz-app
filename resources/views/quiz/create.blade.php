d199c033a9975d0f8530f78ff214de1e06829af7
Asif Khan <asifkhan.github@gmail.com>
2022-09-08 15:23:25 +0600

    create new quiz page modified

--- resources/views/quiz/create.blade.php
+++ resources/views/quiz/create.blade.php
@@ -19,61 +19,24 @@
   <section class="content">
     <form action="" method="post">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
+            <div class="row justify-content-center">
+                <div class="col-8">
                     <div class="card">
                         <div class="card-header">Basic Information</div>
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-4">
+                                <div class="col-8">
                                     <div class="form-group">
-                                        <label for="date">Date <span class="text-danger" title="Required">*</span></label>
-                                        <input type="date" id="date" class="form-control" placeholder="Date">
-                                        <span class="text-danger errorDate"></span>
+                                        <label for="title">Title <span class="text-danger" title="Required">*</span></label>
+                                        <input type="text" id="title" class="form-control" name="title" placeholder="Quiz Title">
+                                        <span class="text-danger errorTitle"></span>
                                     </div>
                                 </div>
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="store">Store <span class="text-danger" title="Required">*</span></label>
-                                        <select id="store" class="form-control select2">
-                                            <option value="">Select Store</option>
-                                        </select>                                              
-                                        <span class="text-danger errorStore"></span>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="supplier">Suppliers <span class="text-danger" title="Required">*</span></label>
-                                        <select id="supplier" class="form-control select2">
-                                            <option value="">Select Supplier</option>
-                                        </select>                                              
-                                        <span class="text-danger errorSupplier"></span>
-                                    </div>
-                                </div>
-                                <div class="col-3">
-                                    <div class="form-group">
-                                        <label for="purchase_status">Purchase Status <span class="text-danger" title="Required">*</span></label>
-                                        <select id="purchase_status" class="form-control select2">
-                                            <option value="">Purchase Status</option>
-                                        </select>                                              
-                                        <span class="text-danger errorPurchaseStatus"></span>
-                                    </div>
-                                </div>
-                                <div class="col-3">
-                                    <div class="form-group">
-                                        <label for="attach_document">Attach Document</label>
-                                        <input type="file" class="form-control"  id="attach_document">
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="form-group">
-                                        <label for="product_selection">Select Product <span class="text-danger" title="Required">*</span></label>
-                                        <div class="form-group">
-                                            <div class="input-group mb-3">
-                                                <div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-barcode"></i></span></div>
-                                                <input type="text" class="form-control" id="product_selection" placeholder="Type product code">
-                                            </div>
-                                        </div>
+                                        <label for="pass_mark">Pass Mark <span class="text-danger" title="Required">*</span></label>
+                                        <input type="number" id="pass_mark" name="pass_mark" class="form-control" placeholder="Pass Mark">
+                                        <span class="text-danger errorPassMark"></span>
                                     </div>
                                 </div>
                             </div>
@@ -81,74 +44,59 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">Order Information</div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-12">
-                                    <label for="">Order Table <span class="text-danger" title="Required">*</span></label>
-                                    <table class="table table-bordered table-striped">
-                                        <thead>
-                                            <th style="width: 610px;">Name</th>
-                                            <th>Code</th>
-                                            <th>Quantity</th>
-                                            <th>Net Unit Cost</th>
-                                            <th>Discount</th>
-                                            <th>Tax</th>
-                                            <th>Sub Total</th>
-                                            <th>Action</th>
-                                        </thead>
-                                        <tbody id="order_table">
-                                            <tr>
-                                                <td style="vertical-align: middle;">A Product</td>
-                                                <td style="vertical-align: middle;text-align: center;">ACB123</td>
-                                                <td><input type="number" class="form-control" value="1" min="1"></td>
-                                                <td style="vertical-align: middle;text-align: center;">1500.50</td>
-                                                <td style="vertical-align: middle;text-align: center;">0</td>
-                                                <td style="vertical-align: middle;text-align: center;">0</td>
-                                                <td style="vertical-align: middle;text-align: center;">1500.0</td>
-                                                <td style="vertical-align: middle;text-align: center;"><button class="btn btn-danger"><i class="fas fa-trash mr-2"></i> Remove</button></td>
-                                            </tr>
-                                        </tbody>
-                                        <tfooter>
-                                            <th style="width: 610px;">Name</th>
-                                            <th>Code</th>
-                                            <th>Quantity</th>
-                                            <th>Net Unit Cost</th>
-                                            <th>Discount</th>
-                                            <th>Tax</th>
-                                            <th>Sub Total</th>
-                                            <th>Action</th>
-                                        </tfooter>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="order_tax">Order Tax</label>
-                                        <select class="form-control select2" id="order_tax">
-                                            <option value="">Order Tax</option>
+            <div class="row justify-content-center">
+                <div class="col-8">
+                    <div id="question_list">
+                        <div class="card qa" id="qa_1">
+                            <div class="card-header">Question & Answers 1</div>
+                            <div class="card-body">
+                                <div class="form-row">                                    
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" name="question1" placeholder="Question">
+                                    </div>
+                                    <div class="col-3">
+                                        <select class="form-control" name="selected_answer1">
+                                            <option value="">Select Answer</option>
+                                            <option value="A">A</option>
+                                            <option value="B">B</option>
+                                            <option value="C">C</option>
+                                            <option value="D">D</option>
                                         </select>
                                     </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="discount">Discount</label>
-                                        <input type="number" class="form-control" id="discount" placeholder="Enter discount">
+                                    <div class="col-3">
+                                        <input type="number" class="form-control" name="mark1" placeholder="Mark">
                                     </div>
                                 </div>
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="shipping_cost">Shipping Cost</label>
-                                        <input type="number" class="form-control" id="shipping_cost" placeholder="Enter shipping cost">
+                                <div class="mt-3">
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">A</div>
+                                        </div>
+                                        <input type="text" class="form-control" name="answer1[]" placeholder="Answer One">
+                                    </div>
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">B</div>
+                                        </div>
+                                        <input type="text" class="form-control" name="answer1[]" placeholder="Answer Two">
+                                    </div>
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">C</div>
+                                        </div>
+                                        <input type="text" class="form-control" name="answer1[]" placeholder="Answer Three">
+                                    </div>
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">D</div>
+                                        </div>
+                                        <input type="text" class="form-control" name="answer1[]" placeholder="Answer Four">
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>  
+                    <button class="btn btn-success float-right" id="add_question" value="1">Add More</button>                  
                 </div>
             </div>
         </div>
