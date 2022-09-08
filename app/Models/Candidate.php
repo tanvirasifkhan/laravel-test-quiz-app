3b0eb9856b4fe254910f34c17522e9886946c231
Asif Khan <asifkhan.github@gmail.com>
2022-09-08 19:20:55 +0600

    model made autheticable

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\QuizParticipation;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Candidate extends Model {
-    use HasFactory;
+class Candidate extends Authenticatable {
+    use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'candidates';
 
     public function quizParticipation(){
