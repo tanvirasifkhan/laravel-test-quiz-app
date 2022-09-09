f27e5eb9523a6b1c0385b067de350a2845b60cb5
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 14:40:07 +0600

    model name bug fixed  for quiz participation

--- app/Models/QuizParticipation.php
+++ app/Models/QuizParticipation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Candidate;
+use App\Models\Quiz;
 
 class QuizParticipation extends Model {
     use HasFactory;
@@ -12,5 +13,9 @@ class QuizParticipation extends Model {
 
     public function candidate(){
         return $this->belongsTo(Candidate::class);
+    }
+
+    public function quiz(){
+        return $this->belongsTo(Quiz::class);
     }
 }
