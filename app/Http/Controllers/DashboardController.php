6088e9710d6877d870b3254bc625eb8ccad196ea
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 17:16:21 +0600

    condition added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,15 +12,30 @@ class DashboardController extends Controller {
     
     // show dashboard page
     public function index(){
-        $count_quiz = Quiz::count();
-        $count_candidate = Candidate::count();
-        $count_approved = Candidate::where('status','approved')->count();
-        $count_pending = Candidate::where('status','pending')->count();
-        $count_participation = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)->count();
-        $count_passed = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)
-            ->where('passed',true)->count();
-        $count_failed = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)
-            ->where('passed',false)->count();
+        $count_quiz = 0;
+        $count_candidate = 0;
+        $count_approved = 0;
+        $count_pending = 0;
+        if(Auth::guard('admin')->check()){
+            $count_quiz = Quiz::count();
+            $count_candidate = Candidate::count();
+            $count_approved = Candidate::where('status','approved')->count();
+            $count_pending = Candidate::where('status','pending')->count();
+        }
+        
+        $count_participation = 0;
+        $count_participation = 0;
+        $count_passed = 0;
+        $count_failed = 0;
+        if(Auth::guard('candidate')->check()){
+            $count_participation = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)->count();
+            $count_participation = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)->count();
+            $count_passed = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)
+                ->where('passed',true)->count();
+            $count_failed = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)
+                ->where('passed',false)->count();
+        }
+        
         return view('dashboard.dashboard',[
             'count_quiz'=>$count_quiz,
             'count_candidate'=>$count_candidate,
