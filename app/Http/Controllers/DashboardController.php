05ae8945172b0492d1a1c2d5e71e4ecfce344ab6
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 17:08:38 +0600

    queries added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,12 +17,18 @@ class DashboardController extends Controller {
         $count_approved = Candidate::where('status','approved')->count();
         $count_pending = Candidate::where('status','pending')->count();
         $count_participation = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)->count();
+        $count_passed = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)
+            ->where('passed',true)->count();
+        $count_failed = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)
+            ->where('passed',false)->count();
         return view('dashboard.dashboard',[
             'count_quiz'=>$count_quiz,
             'count_candidate'=>$count_candidate,
             'count_approved'=>$count_approved,
             'count_pending'=>$count_pending,
-            'count_participation'=>$count_participation
+            'count_participation'=>$count_participation,
+            'count_passed'=>$count_passed,
+            'count_failed'=>$count_failed
         ]);
     }
 }
