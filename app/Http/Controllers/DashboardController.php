52cdc54a661503dda6f22a75681366db7c77dd0c
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 16:42:30 +0600

    dashboard queries added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Quiz;
+use App\Models\Candidate;
+use App\Models\QuizParticipation;
 
 class DashboardController extends Controller {
     
     // show dashboard page
     public function index(){
-        return view('dashboard.dashboard');
+        $count_quiz = Quiz::count();
+        $count_candidate = Candidate::count();
+        $count_approved = Candidate::where('status','approved')->count();
+        $count_pending = Candidate::where('status','pending')->count();
+        $count_participation = QuizParticipation::where('candidate_id',Auth::guard('candidate')->user()->id)->count();
+        return view('dashboard.dashboard',[
+            'count_quiz'=>$count_quiz,
+            'count_candidate'=>$count_candidate,
+            'count_approved'=>$count_approved,
+            'count_pending'=>$count_pending,
+            'count_participation'=>$count_participation
+        ]);
     }
 }
