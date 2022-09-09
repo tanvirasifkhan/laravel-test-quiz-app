5dbfda21ab867967b3de8d82f5e8ce70853996a6
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 11:49:43 +0600

    new participation method created

--- app/Http/Controllers/QuizParticipationController.php
+++ app/Http/Controllers/QuizParticipationController.php
@@ -18,4 +18,10 @@ class QuizParticipationController extends Controller {
         $quiz_list = Quiz::all();
         return view('quiz_participation.quiz_list',['quiz_list'=>$quiz_list]);
     }
+
+    // new quiz participation
+    public function newParticipation($id){
+        $quiz_detail = Quiz::where('id',$id)->get();
+        return view('quiz_participation.new_participation',['quiz_detail'=>$quiz_detail]);
+    }
 }
