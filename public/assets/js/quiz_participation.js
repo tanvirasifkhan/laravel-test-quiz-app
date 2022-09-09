481d202941975cab171e1afc9b3f1648697dbb34
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 13:57:45 +0600

    save participation ajax request added

---
+++ public/assets/js/quiz_participation.js
@@ -0,0 +1,38 @@
+$('#save_participation').click(function(){
+    var quiz_id = $('input[name="quiz_id"]').val()
+    var candidate_id = $('input[name="candidate"]').val()
+    var answer = []
+    var total_mark = 0
+    for(count=0; count<$('input[name="key[]"]').length; count++){
+        var key = Number($('input[name="key[]"]')[count].value)
+        if($('input[name="answer'+ key +'"]:checked').val() == $('input[name="selected_answer[]"]')[count].value){
+            total_mark += Number($('input[name="mark[]"]')[count].value)
+        }
+        answer.push({            
+            'question': $('input[name="question[]"]')[count].value,
+            'selected_answer': $('input[name="selected_answer[]"]')[count].value,
+            'mark': Number($('input[name="mark[]"]')[count].value),
+            'option_answered':$('input[name="answer'+ key +'"]:checked').val()
+        })        
+    }
+
+    var formData = new FormData()
+    formData.append('quiz_id',quiz_id)
+    formData.append('mark',total_mark)
+    formData.append('answer', JSON.stringify(answer))
+
+    $.ajax({
+        method:'POST',
+        url:'/candidate/participation/'+ candidate_id + '/save',
+        headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') },
+        data:formData,
+        dataType:'JSON',
+        contentType:false,
+        processData:false,
+        cache:false,
+        success:function(data){
+            toastr.success('Quiz participation saved successfully!');
+            window.location = '/candidate/participation/list';
+        }
+    })
+});
