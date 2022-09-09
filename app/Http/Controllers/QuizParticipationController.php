d96a27a1a539964ef89f6f9db7ceb17c2e1755f7
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 11:40:15 +0600

    method created for quiz list

--- app/Http/Controllers/QuizParticipationController.php
+++ app/Http/Controllers/QuizParticipationController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\QuizParticipation;
+use App\Models\Quiz;
 
 class QuizParticipationController extends Controller {
     
     // show quiz participation list
     public function index(){
         return view('quiz_participation.list');
+    }
+
+    // quiz list
+    public function quizList(){
+        $quiz_list = Quiz::all();
+        return view('quiz_participation.quiz_list',['quiz_list'=>$quiz_list]);
     }
 }
