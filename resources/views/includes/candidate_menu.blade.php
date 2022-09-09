0a549e0ac4f8553cf35ab2f99dfb9cae73f6dd02
Asif Khan <asifkhan.github@gmail.com>
2022-09-09 14:52:59 +0600

    menu item link added

--- resources/views/includes/candidate_menu.blade.php
+++ resources/views/includes/candidate_menu.blade.php
@@ -1,25 +1,25 @@
 <nav>
     <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-widget="treeview" role="menu" data-accordion="false">
       <li class="nav-item">
-        <a href="dashboard.html" class="nav-link active">
+        <a href="{{ route('candidate.dashboard') }}" class="nav-link {{ Request::is('candidate/dashboard') ? 'active':'' }}">
           <i class="nav-icon fas fa-tachometer-alt"></i>
           <p>Dashboard</p>
         </a>
       </li>
-      <li class="nav-item has-treeview">
-        <a href="#" class="nav-link">
+      <li class="nav-item has-treeview {{ Request::is('candidate/participation/*') ? 'menu-open':'' }}">
+        <a href="#" class="nav-link {{ Request::is('candidate/participation/*') ? 'active':'' }}">
           <i class="nav-icon fas fa-swatchbook"></i>
           <p>Quiz Participation<i class="fas fa-angle-left right"></i></p>
         </a>
         <ul class="nav nav-treeview">
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="{{ route('candidate.participation.list') }}" class="nav-link {{ Request::is('candidate/participation/list') ? 'active':'' }}">
               <i class="far fa-circle nav-icon"></i>
               <p>Participation List</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="{{ route('candidate.participation.quiz_list') }}" class="nav-link {{ Request::is('candidate/participation/quiz/ist') ? 'active':'' }}">
               <i class="far fa-circle nav-icon"></i>
               <p>New Participation</p>
             </a>
