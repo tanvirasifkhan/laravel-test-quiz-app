662837e214914e86659a5738de9bc285f4ec2282
Asif Khan <asifkhan.github@gmail.com>
2022-09-10 10:27:20 +0600

    condition added

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -64,54 +64,64 @@
         </div>
       </div>
     @else
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>{{ $count_quiz }}</h3>
-                <p>Total Quizes</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>{{ $count_participation }}</h3>
-                <p>Total Participation</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>{{ $count_passed }}</h3>
-                <p>Passed</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
+      @if(auth()->guard('candidate')->user()->status == 'pending')
+        <div class="alert alert-warning">
+          Your account is still waiting for approval.You can not do anything until gets approved
+        </div>
+      @elseif(auth()->guard('candidate')->user()->status == 'rejected')
+        <div class="alert alert-danger">
+          Your account is rejected
+        </div>
+      @else
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>{{ $count_quiz }}</h3>
+                  <p>Total Quizes</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-bag"></i>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>{{ $count_failed }}</h3>
-                <p>Failed</p>
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-warning">
+                <div class="inner">
+                  <h3>{{ $count_participation }}</h3>
+                  <p>Total Participation</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
               </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
+            </div>
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3>{{ $count_passed }}</h3>
+                  <p>Passed</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3>{{ $count_failed }}</h3>
+                  <p>Failed</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      @endif
     @endif
   </section>
 @endsection
--- resources/views/includes/candidate_menu.blade.php
+++ resources/views/includes/candidate_menu.blade.php
@@ -6,25 +6,27 @@
           <p>Dashboard</p>
         </a>
       </li>
-      <li class="nav-item has-treeview {{ Request::is('candidate/participation/*') ? 'menu-open':'' }}">
-        <a href="#" class="nav-link {{ Request::is('candidate/participation/*') ? 'active':'' }}">
-          <i class="nav-icon fas fa-swatchbook"></i>
-          <p>Quiz Test<i class="fas fa-angle-left right"></i></p>
-        </a>
-        <ul class="nav nav-treeview">
-          <li class="nav-item">
-            <a href="{{ route('candidate.participation.list') }}" class="nav-link {{ Request::is('candidate/participation/list') ? 'active':'' }}">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Test List</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{ route('candidate.participation.quiz_list') }}" class="nav-link {{ Request::is('candidate/participation/quiz/ist') ? 'active':'' }}">
-              <i class="far fa-circle nav-icon"></i>
-              <p>New Test</p>
-            </a>
-          </li>
-        </ul>
-      </li>
+      @if(auth()->guard('candidate')->user()->status == 'approved')
+        <li class="nav-item has-treeview {{ Request::is('candidate/participation/*') ? 'menu-open':'' }}">
+          <a href="#" class="nav-link {{ Request::is('candidate/participation/*') ? 'active':'' }}">
+            <i class="nav-icon fas fa-swatchbook"></i>
+            <p>Quiz Test<i class="fas fa-angle-left right"></i></p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{ route('candidate.participation.list') }}" class="nav-link {{ Request::is('candidate/participation/list') ? 'active':'' }}">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Test List</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('candidate.participation.quiz_list') }}" class="nav-link {{ Request::is('candidate/participation/quiz/ist') ? 'active':'' }}">
+                <i class="far fa-circle nav-icon"></i>
+                <p>New Test</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+      @endif
     </ul>
 </nav>
